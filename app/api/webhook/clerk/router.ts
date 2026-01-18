c87837d9a8a69056e8b71d948d2b9b516b3799f9
Adeesha <adeeshamx@gmail.com>
2026-01-18 21:47:19 +0530

    Fix mongo

--- app/api/webhook/clerk/router.ts
+++ app/api/webhook/clerk/router.ts
@@ -1,49 +1,47 @@
-import { headers } from "next/headers";
-import { Webhook } from "svix";
-import { connectToDatabase } from "@/lib/mongodb/database";
-import User from "@/lib/mongodb/database/models/user.model";
+import { createUser } from '@/lib/actions/user.actions';
+import { connectToDatabase } from '@/lib/mongodb/database'
+import { clerkClient } from '@clerk/nextjs/server';
+import { verifyWebhook } from '@clerk/nextjs/webhooks'
+import { NextRequest, NextResponse } from 'next/server'
 
-export async function POST(req: Request) {
-  const WEBHOOK_SECRET = process.env.CLERK_WEBHOOK_SECRET;
-
-  if (!WEBHOOK_SECRET) {
-    return new Response("Missing webhook secret", { status: 500 });
-  }
-
-  const payload = await req.text();
-  const headerPayload = await headers();
-
-  const svixHeaders = {
-    "svix-id": headerPayload.get("svix-id")!,
-    "svix-timestamp": headerPayload.get("svix-timestamp")!,
-    "svix-signature": headerPayload.get("svix-signature")!,
-  };
-
-  const wh = new Webhook(WEBHOOK_SECRET);
-
-  let evt: any;
-
+export async function POST(req: NextRequest) {
   try {
-    evt = wh.verify(payload, svixHeaders);
+    const evt = await verifyWebhook(req)
+
+    // Do something with payload
+    // For this guide, log payload to console
+    const { id } = evt.data;
+  const eventType = evt.type;
+ 
+  if(eventType === 'user.created') {
+    const { id, email_addresses, image_url, first_name, last_name, username } = evt.data;
+
+    const user = {
+      clerkId: id,
+      email: email_addresses[0].email_address,
+      username: username!,
+      firstName: first_name!,
+      lastName: last_name!,
+      photo: image_url,
+    }
+
+    const newUser = await createUser(user);
+
+    if(newUser) {
+      const client = await clerkClient();
+      await client.users.updateUserMetadata(id, {
+        publicMetadata: {
+          userId: newUser._id
+        }
+      })
+    }
+
+    return NextResponse.json({ message: 'OK', user: newUser })
+  }
+
+    return new Response('Webhook received', { status: 200 })
   } catch (err) {
-    return new Response("Webhook verification failed", { status: 400 });
+    console.error('Error verifying webhook:', err)
+    return new Response('Error verifying webhook', { status: 400 })
   }
-
-  // Handle user.created
-  if (evt.type === "user.created") {
-    const user = evt.data;
-
-    await connectToDatabase();
-
-    await User.create({
-      clerkId: user.id,
-      email: user.email_addresses[0].email_address,
-      firstName: user.first_name,
-      lastName: user.last_name,
-      username: user.username,
-      imageUrl: user.image_url,
-    });
-  }
-
-  return new Response("OK", { status: 200 });
 }
